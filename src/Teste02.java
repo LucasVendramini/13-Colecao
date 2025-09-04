ee3884af64a62267fb1bdf32bc9185e0a7b7488f
Lucas Vendramini Lubianco <lucasvlubianco@gmail.com>
2025-09-04 09:28:39 -0300

    Update Teste02.java

--- src/Teste02.java
+++ src/Teste02.java
@@ -6,11 +6,11 @@ public class Teste02 {
 
        Set<Aluno> lista = new HashSet<>();
 
-        lista.add(new Aluno(556695, "Gayse Molestado", "SI"));
-        lista.add(new Aluno(556736, "Enzo Bilola", "SI"));
-        lista.add(new Aluno(556161, "Lucas Picudao", "SI"));
-        lista.add(new Aluno(557645, "Julia Narizin", "SI"));
-        lista.add(new Aluno(556683, "Rebeca Danadinha", "SI"));
+        lista.add(new Aluno(556695, "Gabriel Jose", "SI"));
+        lista.add(new Aluno(556736, "Enzo Cezar", "SI"));
+        lista.add(new Aluno(556161, "Lucas Vendramini", "SI"));
+        lista.add(new Aluno(557645, "Julia Mendes", "SI"));
+        lista.add(new Aluno(556683, "Rebeca Berbert", "SI"));
 
         lista.forEach(aluno -> System.out.println(aluno));
     }
